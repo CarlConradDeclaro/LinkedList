4456c2b33b40c8220a73f3f44d6cc848f45c1dce
carlconrad <carlconrad98071234@gmail.com>
2023-09-05 09:17:51 +0800

    Add & Search

---
+++ LinkedList.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.retailstore;
+
+/**
+ *
+ * @author carlconrad
+ */
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+    this.data =  data;
+    next=  null;
+    }
+}
+
+class List{
+   
+    Node head;
+       int data;
+       Node next;
+
+
+    void add(int data){
+    Node node = new Node(data);
+    node.next = head;
+    head = node;
+    }
+   
+    void print(){
+     
+        Node n = head;
+        
+        while(n!= null){
+        System.out.println(n.data);
+        n = n.next;
+         }
+    }
+    
+    boolean search(int x){
+      Node n =head;
+      
+      while(n.next != null){
+        if(n.data == x){
+          return true;
+        }
+        n = n.next;
+      }
+      return false;
+             
+    }
+    
+ 
+}
+
+
+
+public class LinkedList {
+      public static void main(String[] args) {
+        
+          List n =  new List();
+          n.add(2);
+          n.add(3);
+          n.add(5);
+         
+          n.print();
+          
+          System.out.println(n.search(53));
+      }
+}
